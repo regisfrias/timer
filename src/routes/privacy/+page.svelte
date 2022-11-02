06e679f2388ed221065c64a60af35541025256f4
Régis Frias <regisfrias@gmail.com>
2022-11-02 16:47:45 +0200

    Text.

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -5,5 +5,5 @@
 <Layout>
   <h1>Privacy</h1>
   <p>This site saves data to your browser's <a href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage">local storage</a>. This data is not uploaded anywhere and is only used so that you can pick up where you left off.</p>
-  <p>The home page footer has a button for deleting all this data. You can also inspect it or delete it from your broser's developer tools, under <em>application</em> > <em>local storage</em> > <em>[this domain]</em></p>
+  <p>The home page footer has a button for deleting all this data. You can also inspect it or delete it from your broser's developer tools, under <em>application</em> > <em>local storage</em> > <em>https://timer.regisfrias.com/</em></p>
 </Layout>
