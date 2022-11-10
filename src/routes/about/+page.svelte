40ae3e7ad39e918e4744fc64e132f7ef2f2c9718
Régis Frias <regisfrias@gmail.com>
2022-11-10 14:35:16 +0200

    Update about text.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,6 +5,6 @@
 <Layout>
   <h1>About</h1>
   <p>This is just a simple working hours time tracker for my personal use. I created it because I couldn't find any application that suited my needs and/or whose privacy terms I could agree with.</p>
-  <p>Feel free to use it. You don't have to worry about data privacy because <a href="/privacy">it stores everything locally</a>.</p>
+  <p>Feel free to use it. You don't have to worry about data privacy because <a href="/privacy">it stores everything locally</a>. This also means that if you're using different machines the data will be different. If you want to share data across devices you need to export it from one and import it on the other</p>
   <p>You can find out more about me <a href="https://regisfrias.com/">here</a>.</p>
 </Layout>
